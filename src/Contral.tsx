75eb626a9719baa6368440af280dbc31b04874fc
Username_Full <tohowtodoit@gmail.com>
2021-10-30 20:20:26 +0800

    t

--- src/Contral.tsx
+++ src/Contral.tsx
@@ -16,12 +16,12 @@ import React, { useState, useEffect } from "react";
 
 const Contral: React.FC = (props) => {
   const [formData, setFormData] = useState({
-    a: 10,
-    b: 0,
-    c: 0,
-    d: 0,
-    e: 0,
-    f: 0,
+    a: "0",
+    b: "0",
+    c: "0",
+    d: "0",
+    e: "0",
+    f: "0",
   });
   const getrate = (e) => {
     //   fetch()
@@ -49,70 +49,52 @@ const Contral: React.FC = (props) => {
         <Box>
           <InputGroup>
             <InputLeftAddon p={1} fontSize="sm" children="湿度原始值" />
-            <NumberInput
-              value={formData.a}
-              onChange={(e) => setFormData({ ...formData, a: e.target.value })}
-            >
-              <NumberInputField />
-            </NumberInput>
+            <Input value={formData.a}
+              onChange={(e) => setFormData({ ...formData, a:e.target.value })}
+              />
           </InputGroup>
         </Box>
         <Box>
           <InputGroup>
             <InputLeftAddon p={1} fontSize="sm" children="湿度斜率" />
-            <NumberInput
-              value={formData.b}
-              onChange={(e) => setFormData({ ...formData, b: e.target.value })}
-            >
-              <NumberInputField />
-            </NumberInput>
+            <Input value={formData.b}
+              onChange={(e) => setFormData({ ...formData, b:e.target.value })}
+              />
           </InputGroup>
         </Box>
         <Box>
           <InputGroup>
             <InputLeftAddon p={1} fontSize="sm" children="甲醛原始值" />
-            <NumberInput
-              value={formData.c}
-              onChange={(e) => setFormData({ ...formData, c: e.target.value })}
-            >
-              <NumberInputField />
-            </NumberInput>
+            <Input value={formData.c}
+              onChange={(e) => setFormData({ ...formData, c:e.target.value })}
+              />
           </InputGroup>
         </Box>
         <Box>
           {" "}
           <InputGroup>
             <InputLeftAddon p={1} fontSize="sm" children="甲醛斜率" />
-            <NumberInput
-              value={formData.d}
-              onChange={(e) => setFormData({ ...formData, d: e.target.value })}
-            >
-              <NumberInputField />
-            </NumberInput>
+            <Input value={formData.d}
+              onChange={(e) => setFormData({ ...formData, d:e.target.value })}
+              />
           </InputGroup>
         </Box>
         <Box>
           {" "}
           <InputGroup>
             <InputLeftAddon p={1} fontSize="sm" children="乙醇原始值" />
-            <NumberInput
-              value={formData.e}
-              onChange={(e) => setFormData({ ...formData, e: e.target.value })}
-            >
-              <NumberInputField />
-            </NumberInput>
+            <Input value={formData.e}
+              onChange={(e) => setFormData({ ...formData, e:e.target.value })}
+              />
           </InputGroup>
         </Box>
         <Box>
           {" "}
           <InputGroup>
             <InputLeftAddon p={1} fontSize="sm" children="乙醇斜率" />
-            <NumberInput
-              value={formData.f}
-              onChange={(e) => setFormData({ ...formData, f: e.target.value })}
-            >
-              <NumberInputField />
-            </NumberInput>
+            <Input value={formData.f}
+              onChange={(e) => setFormData({ ...formData, f:e.target.value })}
+              />
           </InputGroup>
         </Box>
       </SimpleGrid>
